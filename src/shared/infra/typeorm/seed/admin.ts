f098fc9fc0c29ef854cab3eb280def833d23acb6
Davi Pessoa <davialvespessoa@gmail.com>
2023-01-31 10:52:44 -0300

    Criando seed de usuário

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,21 @@
+import { hash } from "bcryptjs";
+import { v4 as uuidV4 } from "uuid";
+
+import createConnection from "../index";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuidV4();
+  const passowrd = await hash("admin", 8);
+
+  await connection.query(
+    `INSERT INTO users(id, name, email, password, "isAdmin", created_at, driver_license)
+      values('${id}', 'admin', 'admin@rentx.com.br', '${passowrd}', true, 'now()', 'XXXXXX')
+    `
+  );
+
+  await connection.close;
+}
+
+create().then(() => console.log("user admin created!"));
