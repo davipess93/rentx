23acef81bf1282356e7d2699efaf76b1a7674070
Davi Pessoa <davialvespessoa@gmail.com>
2023-02-02 15:15:04 -0300

    Trabalhando com datas com dayjs

--- src/modules/rentals/useCase/CreateRentalUseCase.ts
+++ src/modules/rentals/useCase/CreateRentalUseCase.ts
@@ -1,7 +1,12 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
 import { AppError } from "@shared/errors/AppError";
 
 import { Rental } from "../infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../repositories/IRentalsRepository";
+
+dayjs.extend(utc);
 
 interface IRequest {
   user_id: string;
@@ -17,6 +22,8 @@ class CreateRentalUseCase {
     car_id,
     expected_return_date,
   }: IRequest): Promise<Rental> {
+    const minimumHour = 24;
+
     const carUnavailable = await this.rentalRepository.findOpenRentalByCar(
       car_id
     );
@@ -31,6 +38,19 @@ class CreateRentalUseCase {
 
     if (rentalOpenToUser) {
       throw new AppError("There's a rental in progress for user!");
+    }
+
+    const expectedReturnDateFormat = dayjs(expected_return_date)
+      .utc()
+      .local()
+      .format();
+
+    const dateNow = dayjs().utc().local().format();
+
+    const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours");
+
+    if (compare < minimumHour) {
+      throw new AppError("Invalid return time!");
     }
 
     const rental = this.rentalRepository.create({
