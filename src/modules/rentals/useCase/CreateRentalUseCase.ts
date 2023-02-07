0bd371c8a7222b7a0135e46b7e402f4a2656cc50
Davi Pessoa <davialvespessoa@gmail.com>
2023-02-07 16:28:56 -0300

    Corrigindo status de um carro

--- src/modules/rentals/useCase/CreateRentalUseCase.ts
+++ src/modules/rentals/useCase/CreateRentalUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { IDateProvider } from "@shared/container/provider/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
@@ -18,7 +19,9 @@ class CreateRentalUseCase {
     @inject("RentalsRepository")
     private rentalRepository: IRentalsRepository,
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
   ) {}
 
   async execute({
@@ -60,6 +63,8 @@ class CreateRentalUseCase {
       car_id,
       expected_return_date,
     });
+
+    await this.carsRepository.updateAvailable(car_id, false);
 
     return rental;
   }
