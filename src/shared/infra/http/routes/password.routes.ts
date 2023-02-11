22e98175b1c4e644ed4d7cd6740e12131ddd68b9
Davi Pessoa <davialvespessoa@gmail.com>
2023-02-10 21:47:49 -0300

    Caso de uso do reset de senha

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,10 +1,14 @@
-import { SendFortPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 import { Router } from "express";
+
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
+import { SendFortPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendFortPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetPasswordUserController.handle);
 
 export { passwordRoutes };
